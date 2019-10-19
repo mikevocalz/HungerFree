01c5ab756610a47cd530bc405d8a7c56d281b244
Victoria Mei <vm930@hotmail.com>
2019-10-19 10:53:59 -0400

    add mapview with style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,34 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
+import MapView from 'react-native-maps';
 
 export class HomeScreen extends Component {
-  static navigationOptions = {
-    title: "Home"
-    //header: null
-  };
+	static navigationOptions = {
+		title: 'Home'
+		//header: null
+	};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Home Component </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<ScrollView style={{ flex: 1 }}>
+					<Text>Home Component </Text>
+					<MapView showsUserLocation={true} style={styles.mapStyle} />
+				</ScrollView>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center"
-  }
+	container: {
+		flex: 1,
+		alignItems: 'center'
+	},
+	mapStyle: {
+		width: Dimensions.get('window').width,
+		height: 400
+	}
 });
 
 export default HomeScreen;
