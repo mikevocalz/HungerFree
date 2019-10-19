a3224bf6971dc6e3fc0ac62328dd7936b64b5341
Victoria Mei <vm930@hotmail.com>
2019-10-19 12:26:17 -0400

    getting current user location with states

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Dimensions, ScrollView } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Location } from 'expo';
+import Permissions from 'expo-permissions';
 
 export class HomeScreen extends Component {
 	static navigationOptions = {
@@ -8,12 +10,49 @@ export class HomeScreen extends Component {
 		//header: null
 	};
 
+	state = {
+		location: '',
+		errorMessage: '',
+		latitude: null,
+		longitude: null
+	};
+
+	componentWillMount() {
+		this.getLocation();
+	}
+
+	getLocation = async () => {
+		try {
+			await Permissions.askAsync(Permissions.LOCATION);
+			const location = await Location.getCurrentPostionAsync();
+			this.setState({ location });
+			console.log('hi');
+		} catch (e) {
+			console.log('hihi');
+			console.log(e);
+			this.setState({
+				errorMessage: 'PREMISSION NOT GRANTED'
+			});
+		}
+	};
+
 	render() {
 		return (
 			<View style={styles.container}>
 				<ScrollView style={{ flex: 1 }}>
 					<Text>Home Component </Text>
-					<MapView showsUserLocation={true} style={styles.mapStyle} />
+					<Text>{JSON.stringify(this.state.location)}</Text>
+					<MapView
+						showsUserLocation={true}
+						region={{
+							latitudeDelta: 0.0922,
+							longitudeDelta: 0.0421,
+							latitude: 40.731031,
+							longitude: -73.991324
+						}}
+						style={styles.mapStyle}
+						provider={PROVIDER_GOOGLE}
+					/>
 				</ScrollView>
 			</View>
 		);
